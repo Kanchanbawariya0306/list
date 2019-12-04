ca3a2aa6ca8fad315517c66ea2e8b90234feed77
Kanchanbawariya0306 <54577596+Kanchanbawariya0306@users.noreply.github.com>
2019-12-04 23:12:36 +0530

    Create liststtstts.py

---
+++ liststtstts.py
@@ -0,0 +1,10 @@
+
+  print ("\t\t\LIST ")
+a = ['arun ','ahirwar','ravi','ahirwar ','munna ','ahirwar'] 
+print("List = ",a)   
+a.append(62638736)
+print("List = ",a) 	
+a.extend(5:25)
+print("List = ",a)
+a.pop()
+print("List = ",a)
